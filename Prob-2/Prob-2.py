0934da09ff34b41d82819cfa5177b48aa973d28c
Rmballenger <R.ballenger@students.clark.edu>
2020-05-19 00:56:53 -0700

    Finished Assignment!

--- Prob-2/Prob-2.py
+++ Prob-2/Prob-2.py
@@ -2,14 +2,133 @@
 #   Programming Assignment 5
 #       Prob-2.py
 
-# <YOUR NAME>
+# Robert Ballenger
 
 # IPO
+# Input: Total cost of the transaction and the Amount Tendered
+# Process: Subtract cost from tendered, get change amount, and calculate the denominations
+# Output: Cost, Tendered, Change, and display the denominations listed out.
 
-# function definition
+
+# Here at the start of our program, we have a main function that assigns two variables with
+# the user input and assigns them both as float. Then I send the variables to a new function.
+def main():
+
+    costOfItem = float(input("What was the cost of the item? "))
+    amountPaid = float(input("How much did the customer pay? "))
+
+    changeDue(costOfItem, amountPaid)
+
+# In this new function, I draw the two variables from before. More details in comments within.
 
 
-# main function definition
+def changeDue(costOfItem, amountPaid):
+
+    # To start, I assign some variables. I did this originally to make it less confusing but in truth maybe the raw numbers would have worked just the same.
+    hundred = 100
+    fifty = 50
+    twenty = 20
+    ten = 10
+    five = 5
+    one = 1
+    quarter = .25
+    dime = .10
+    nickel = .05
+    penny = .01
+
+    # Next we find out the total in change being returned.
+    totalDue = amountPaid - costOfItem
+
+    # Now we begin the long run of going down one by one, dividing the total due by the denomination were looking at.
+    # Then I create a new 'total' by subtracting whatever might have been removed in finding the amount of denomination used.
+    hundredsReturened = totalDue / hundred
+    minusHundreds = totalDue - (int(hundredsReturened) * hundred)
+
+    fiftiesReturned = minusHundreds / fifty
+    minusFifties = minusHundreds - (int(fiftiesReturned) * fifty)
+
+    twentysReturned = minusFifties / twenty
+    minusTwentys = minusFifties - (int(twentysReturned) * twenty)
+
+    tensReturned = minusTwentys / ten
+    minusTens = minusTwentys - (int(tensReturned) * ten)
+
+    fivesReturned = minusTens / five
+    minusFives = minusTens - (int(fivesReturned) * five)
+
+    onesReturned = minusFives / one
+    minusOnes = minusFives - (int(onesReturned) * one)
+
+    quartersReturned = minusOnes / quarter
+    minusQuarters = minusOnes - (int(quartersReturned) * quarter)
+
+    dimesReturned = minusQuarters / dime
+    minusDimes = minusQuarters - (int(dimesReturned) * dime)
+
+    nickelsReturned = minusDimes / nickel
+    minusNickels = minusDimes - (int(nickelsReturned) * nickel)
+
+    penniesReturned = minusNickels / penny
+
+    # Last we create a new function which a WHOLE BUNCH of variables passed with it.
+    outputFunction(costOfItem, amountPaid, totalDue, int(hundredsReturened), int(fiftiesReturned), int(twentysReturned), int(tensReturned), int(fivesReturned),
+                   int(onesReturned), int(quartersReturned), int(dimesReturned), int(nickelsReturned), int(penniesReturned))
+
+
+# Into that function, we issue a print statement and go through a if/else loop to list what change is being returned as we don't need to see 0 tens if there's no tens. (Unless that's what you wanted in which case I'd just remove the if/else setup and it would issue the 0's too.)
+def outputFunction(costOfItem, amountPaid, totalDue, hundredsReturened, fiftiesReturned, twentysReturned, tensReturned, fivesReturned, onesReturned, quartersReturned, dimesReturned, nickelsReturned, penniesReturned):
+    print("Cost: $", costOfItem, "\tTendered: $",
+          amountPaid, "\tChange: $", "%.2f" % totalDue)
+
+    if hundredsReturened > 0:
+        print(hundredsReturened, " hundreds")
+    else:
+        pass
+
+    if fiftiesReturned > 0:
+        print(fiftiesReturned, " fifties")
+    else:
+        pass
+
+    if twentysReturned > 0:
+        print(twentysReturned, " twenties")
+    else:
+        pass
+
+    if tensReturned > 0:
+        print(tensReturned, " tens")
+    else:
+        pass
+
+    if fivesReturned > 0:
+        print(fivesReturned, " fives")
+    else:
+        pass
+
+    if onesReturned > 0:
+        print(onesReturned, " ones")
+    else:
+        pass
+
+    if quartersReturned > 0:
+        print(quartersReturned, " quarters")
+    else:
+        pass
+
+    if dimesReturned > 0:
+        print(dimesReturned, " dimes")
+    else:
+        pass
+
+    if nickelsReturned > 0:
+        print(nickelsReturned, " nickels")
+    else:
+        pass
+
+    if penniesReturned > 0:
+        print(penniesReturned, " pennies")
+    else:
+        pass
 
 
 main()
--- Prob-3/Prob-3.py
+++ Prob-3/Prob-3.py
@@ -2,14 +2,58 @@
 #   Programming Assignment 5
 #       Prob-3.py
 
-# <YOUR NAME>
+# Robert Ballenger
 
 # IPO
+# Input - Get the size of the job in square feet, and the price for a gallon of paint.
+# Process - All the math garbage. Bleh I hate math.
+# Output - Print's a list for the user including: Number of gallons of paint required, Hours of labor required, Cost of the paint, Labor Charges, and Total cost of the job.
+#
+# Data: 112 Square feet per 1 gallon and 8 hours of labor
+#
 
-# function definition
+
+# Is there a reason to have the main function listed last? I see it frequently but feel it being first makes more sense. Either way, couple of variables being created here from user input. I force it to store them as int and float as I need a whole number for one and a decimal for the other.
+# Then I call the next function, sending the variables I've just made over.
+def main():
+    print("Howdy, thanks for choosing Professional Finger Painting\nThe only painting company that paints truely by 'hand'!")
+    squareFootage = int(
+        input("To start, please enter how many square feet you need painted: "))
+    pricePaint = float(
+        input("Great! Next, enter the cost of the paint you want to use: "))
+
+    costEstimate(squareFootage, pricePaint)
 
 
-# main function definition
+# The function that does all the work. Bringing in the variables from before and run the basic math. Detailed comments within to explain my choices.
+def costEstimate(squareFootage, pricePaint):
+
+    # For this I need a variable for the number of gallons of paint> I run the sqft variable from before and divide it by 112 which is the wall space per gallon. After, I run a if statement as putting a sqft less than 112 resulted in 0 gallons of paint required.
+    gallonsOfPaint = squareFootage / 112
+    if gallonsOfPaint < 1:
+        gallonsOfPaint = 1
+
+    # Few more variables in the same mannor. Using 8 for the hours per gallon, and 35 for dollars per hour worked. Last a 99 dollar edition for the one time setup fee.
+    hoursOfLabor = gallonsOfPaint * 8
+    totalPricePaint = gallonsOfPaint * pricePaint
+    totalLaborCost = hoursOfLabor * 35
+    overallTotalCost = totalPricePaint + totalLaborCost + 99
+
+    # I don't know how to send new variables BACK to the main() function, so I'm creating a new one and sending the variables I have now over to it.
+    printReceipt(gallonsOfPaint, hoursOfLabor, totalPricePaint,
+                 totalLaborCost, overallTotalCost)
+
+
+# Last function here of the program, taking the variables from before, I toss in a couple print statements to make it readable for the user.
+def printReceipt(gallonsOfPaint, hoursOfLabor, totalPricePaint, totalLaborCost, overallTotalCost):
+    print("Sounds great! We can total that up for you here as an itemized receipt!\n")
+    print("Gallons of Paint required:", int(gallonsOfPaint))
+    print("Hours to complete the job:", hoursOfLabor)
+    print("Total price of the paint:", totalPricePaint)
+    print("Labor charges:\t\t", totalLaborCost)
+    print("One time setup fee: \t 99")
+    print("--------------------------------")
+    print("Your total cost: \t $", overallTotalCost)
 
 
 main()
